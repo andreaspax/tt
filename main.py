6b6145c505abee45757f859f600ae2dade2507af
Andreas <49064513+andreaspax@users.noreply.github.com>
2025-02-03 14:22:00 +0000

    updating main.py

--- main.py
+++ main.py
@@ -3,16 +3,28 @@ import fastapi
 
 app = fastapi.FastAPI()
 
-@app.get('/')
+@app.post('/option')
 
-def index (option):
+async def index(request: Request):
+    body = await request.json()
+    option = body["option"]
+
     options = {1: "Hi there",
             2: "Hola amigo",
             3: "Salut amie",
-            4: "Ciao tutti"}
-    print("You selected '"+option+"'")
+            4: "Ciao tutti",}
+    print("You selected '" + str(option) + "'")
+    
+    greeting_message = options[option]
+    return {"greeting":greeting_message}
 
-    return print(options[option])
 
+if __name__ == "__main__":
+    # Word you want to find the closest embedding for
+    option = 2
 
+   
+    return_message = index(option) 
 
+    # Print the result
+    print(f"Option '{option}' was selected: '{return_message}'")
