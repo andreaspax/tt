cd3d81402a81136a2cc6ec09f90dbb447aa6d561
Andreas <49064513+andreaspax@users.noreply.github.com>
2025-02-03 13:16:08 +0000

    adding requirements.txt file for deployment

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
 #this is a deployment test file
+import fastapi
+
+app = fastapi.FastAPI()
+
+@app.get('/')
+
+def index ():
+    return {1: "Hi there",
+            2: "Hola amigo",
+            3: "Salut amie",
+            4: "Ciao tutti"}
+
+
